ff295abef20416b9cb7849471afef0d67498e034
Kevin Reid <kpreid@switchb.org>
2009-07-17 19:13:56 +0000

    E-on-JavaScript: Updoc improvements: * "Rerun" button to allow rerunning script without reloading (for use in debugging) * More refactoring into methods and local functions in the updoc driver; there is now a function dedicated to setting the appearance (via class) of an updoc step and progress bar element in sync. * Bug fix: println() acted as print() because makePrintOrPrintln and its callers were inconsistent in the expected parameters.

--- serve/updoc-driver.js
+++ serve/updoc-driver.js
@@ -21,11 +21,9 @@ function makePrintFunc(unenv) {
   return printEFunc;
 }
 
-function updoc_Driver(prefix) {
-  var waitHook = "arbitrary resolved value";
-  
-  // XXX const map should be a CycleBreaker instead
-  var printFunc = makePrintFunc(new e_ConstMap([], []));
+function updoc_Driver(prefix, runInitFunc) {
+  var waitHook;
+  var printFunc;
   
   var interp = e_cajita.freeze({
     emsg_waitAtTop_1: function (ref) { 
@@ -44,13 +42,16 @@ function updoc_Driver(prefix) {
   
   function makePrintOrPrintln(writer, ln) {
     return e_cajita.freeze({
+      emsg___printOn_1: function (out) {
+        e_call(out, "write", ["<print" + (ln ? "ln" : "") + ">"]);
+      },
       emsg: function (verb, args) { 
         if (verb === "run") {
-          e_call(stdoutWB[0], "printAll", [args]);
-          if (ln) e_call(stdoutWB[0], "println", []);
+          e_call(writer, "printAll", [args]);
+          if (ln) e_call(writer, "println", []);
           return e_null;
         } else {
-          return e_noSuchMethod(this, verb, args)
+          e_doMiranda(functionObject, verb, args, e_noSuchMethod);
         }
       },
     });
@@ -64,22 +65,88 @@ function updoc_Driver(prefix) {
     e_call(buffer, "_clear", []); // XXX ad hoc invented on the spot
   }
   
+  function getRunFuncForStep(index) {
+    return window[prefix + "_runOuter_" + index];
+  }
+  
+  function doNotifyStatus() {
+    for (var i = 0; i < updoc_notifyStatus.length; i++) {
+      e_call(e_e, "send", [updoc_notifyStatus[i], "run", [prefix]])
+    }
+  }
+  
+  function finishStep(index, answers, expectedAnswers) {
+    if (window["console"]) console.log(prefix + ": updoc step " + index + " \\ finished");
+    var stepOutput = document.getElementById(prefix + "-output-" + index);
+
+    var match = expectedAnswers.length == answers.length;
+    for (var j = 0; j < answers.length; j++) {
+      var a = answers[j];
+      var exp = expectedAnswers[j];
+      stepOutput.appendChild(document.createTextNode('# ' + a[0] + ': ' + a[1] + '\n\n'));
+      if (a[2] !== undefined) {
+        stepOutput.appendChild(document.createTextNode(a[2] + '\n\n'));
+      }
+
+      if (match && (   a[0] != exp[0]
+                    || a[1] != exp[1])) {
+        stepOutput.appendChild(document.createTextNode("fail at " + j + "-" + (a[0] != exp[0]) + "-" + (a[1] != exp[1]) + " lenGot=" + a[1].length + "=<<" + a[1] + ">> lenExp=" + exp[1].length + "=<<" + exp[1] + ">>"));
+        match = false;
+      }
+    }
+
+    setStepStatus(index, match ? "updoc-matched" : "updoc-mismatch");
+
+    var statusRecord = updoc_status[prefix];
+
+    statusRecord[match ? "success" : "failure"]++;
+    doNotifyStatus();
+  }
+
+  // XXX this does *not* include updating the interframe-communication status record
+  function setStepStatus(index, statusClass) {
+    var stepFrame = document.getElementById(prefix + "-step-" + index);
+    var stepProgress = document.getElementById(prefix + "-progress-" + index);
+
+    stepFrame.className = "updoc-step " + statusClass;
+    stepProgress.className = "updoc-progress " + statusClass;
+  }
+  
   var driver = {
     "augmentEnv": function (env) {
       var makeVerbFacet = env.emsg_get_1("__makeVerbFacet");
       env = e_call(env, "with", ["interp", interp]);
       env = e_call(env, "with", ["stdout", stdoutWB[0]]);
       env = e_call(env, "with", ["stderr", stderrWB[0]]);
-      env = e_call(env, "with", ["print", makePrintOrPrintln(false)]);
-      env = e_call(env, "with", ["println", makePrintOrPrintln(true)]);
+      env = e_call(env, "with", ["print", makePrintOrPrintln(stdoutWB[0], false)]);
+      env = e_call(env, "with", ["println", makePrintOrPrintln(stdoutWB[0], true)]);
       return env;
     },
     "run": function () {
+      // reset internal and external updoc state
+      runInitFunc();
+      waitHook = "arbitrary resolved value";
+      printFunc = makePrintFunc(new e_ConstMap([], [])); // XXX const map should be a CycleBreaker instead
+      
+      // initialize and reset step record
       updoc_status[prefix] = {done: false, success: 0, failure: 0};
+      doNotifyStatus();
+      
+      // start updoc
       setTimeout(function () { driver.chainStep(0) }, 1);
+      
+      // Reset appearance of all steps
+      for (var index = 0; getRunFuncForStep(index) !== undefined; index++) {
+        setStepStatus(index, "updoc-pending");
+
+        var stepOutput = document.getElementById(prefix + "-output-" + index);
+        var c;
+        while (c = stepOutput.firstChild) stepOutput.removeChild(c);
+      }
+      
     },
     "chainStep": function (stepIndex) {
-      var runFunc = window[prefix + "_runOuter_" + stepIndex];
+      var runFunc = getRunFuncForStep(stepIndex);
       if (runFunc !== undefined) {
         var callback = runFunc(driver);
         e_slot_Ref.emsg_get_0().emsg_whenResolved_2(waitHook, e_wrapJsFunction(function () {
@@ -88,18 +155,17 @@ function updoc_Driver(prefix) {
         }));
       } else {
         updoc_status[prefix].done = true;
-        updoc_doNotifyStatus(prefix);
+        doNotifyStatus();
       }
     },
     "runSyntaxError": function (prefix, errorString, index, expectedAnswers) {
-      updoc_finishStep(prefix, index, [["syntax error", errorString]], expectedAnswers);
+      finishStep(index, [["syntax error", errorString]], expectedAnswers);
       return function () {};
     },
 
     "runStep": function (prefix, func, index, expectedAnswers) {
-      //console.log(prefix + ": updoc step " + index + " / started");
-      var stepFrame = document.getElementById(prefix + "-step-" + index);
-      stepFrame.className = "updoc-step updoc-running";
+      if (window["console"]) console.log(prefix + ": updoc step " + index + " / started");
+      setStepStatus(index, "updoc-running");
 
       // 3rd element is not compared but is printed, if defined
       var answers = [];
@@ -121,46 +187,10 @@ function updoc_Driver(prefix) {
         captureOutput(answers, "stdout", stdoutWB[1]);
         captureOutput(answers, "stderr", stderrWB[1]);
 
-        updoc_finishStep(prefix, index, answers, expectedAnswers);
+        finishStep(index, answers, expectedAnswers);
       };
     }
   };
   return driver;
 }
 
-function updoc_finishStep(prefix, index, answers, expectedAnswers) {
-  //console.log(prefix + ": updoc step " + index + " \\ finished");
-  var stepFrame = document.getElementById(prefix + "-step-" + index)
-  var stepProgress = document.getElementById(prefix + "-progress-" + index)
-  var stepOutput = document.getElementById(prefix + "-output-" + index)
-  
-  var match = expectedAnswers.length == answers.length
-  for (var j = 0; j < answers.length; j++) {
-    var a = answers[j]
-    var exp = expectedAnswers[j]
-    stepOutput.appendChild(document.createTextNode('# ' + a[0] + ': ' + a[1] + '\n\n'))
-    if (a[2] !== undefined) {
-      stepOutput.appendChild(document.createTextNode(a[2] + '\n\n'))
-    }
-    
-    if (match && (   a[0] != exp[0]
-                  || a[1] != exp[1])) {
-      //stepOutput.appendChild(document.createTextNode("fail at " + j + "-" + (a[0] != exp[0]) + "-" + (a[1] != exp[1]) + " lenGot=" + a[1].length + "=<<" + a[1] + ">> lenExp=" + exp[1].length + "=<<" + exp[1] + ">>"));
-      match = false;
-    }
-  }
-
-  stepFrame.className = match ? "updoc-step updoc-matched" : "updoc-step updoc-mismatch"
-  stepProgress.className = match ? "updoc-progress updoc-matched" : "updoc-progress updoc-mismatch"
-  
-  var statusRecord = updoc_status[prefix];
-  
-  statusRecord[match ? "success" : "failure"]++;
-  updoc_doNotifyStatus(prefix)
-}
-
-function updoc_doNotifyStatus(prefix) {
-  for (var i = 0; i < updoc_notifyStatus.length; i++) {
-    e_call(e_e, "send", [updoc_notifyStatus[i], "run", [prefix]])
-  }
-}
